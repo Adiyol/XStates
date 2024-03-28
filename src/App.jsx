eecc62d299749eb9467e507c643c337a9051178d
lolers <adithyajayan@gmail.com>
2024-03-28 21:58:52 +0530

    Xstates

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import styles from "./App.module.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [states, setStates] = useState([]);
+  const [selectedState, setSelectedState] = useState("");
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+
+  useEffect(() => {
+    async function getCountries() {
+      let response = await fetch(
+        "https://crio-location-selector.onrender.com/countries"
+      );
+      let responseJSON = await response.json();
+      console.log(responseJSON, "responseJSON");
+      setCountries(responseJSON);
+    }
+    getCountries();
+  }, []);
+
+  useEffect(() => {
+    async function getStates() {
+      let response = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+      );
+      let responseJSON = await response.json();
+      setStates(responseJSON);
+    }
+    if (selectedCountry !== "") {
+      getStates();
+    }
+
+    setSelectedState("");
+    setSelectedCity("");
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    async function getCities() {
+      let response = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+      );
+      let responseJSON = await response.json();
+      setCities(responseJSON);
+    }
+    if (selectedState !== "") {
+      getCities();
+    }
+    setSelectedCity("");
+  }, [selectedState]);
+
+  return (
+    <>
+      <div className={styles.center}>
+        <h1>Select Location</h1>
+        <div>
+          <select
+            value={selectedCountry}
+            onChange={(e) => {
+              console.log("running");
+              setSelectedCountry(e.target.value);
+            }}
+          >
+            <option value="">Select Country</option>
+            {countries.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selectedState}
+            onChange={(e) => {
+              setSelectedState(e.target.value);
+            }}
+          >
+            <option value="">Select State</option>
+            {states.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selectedCity}
+            onChange={(e) => {
+              setSelectedCity(e.target.value);
+            }}
+          >
+            <option value="">Select City</option>
+            {cities.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+        {selectedCity && <p>you selected <span style={{fontSize: "20px", fontWeight: "bold"}}>{selectedCity}</span>, {selectedState}, {selectedCountry}</p>}
+      </div>
+    </>
+  );
+}
+
+export default App;
